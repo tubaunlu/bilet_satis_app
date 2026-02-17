09ce85fd0fe7186a013dfcfa4d5aecd1ffcb78db
tubaunlu <tuba.unlu.011@gmail.com>
2026-02-17 17:56:43 +0300

    update
    commit

--- lib/auth/register/register_page.dart
+++ lib/auth/register/register_page.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
 
+
 class RegisterPage extends StatefulWidget {
      const RegisterPage({super.key});
 
@@ -7,79 +8,27 @@ class RegisterPage extends StatefulWidget {
     State<RegisterPage> createState() => _RegisterPageState();  
   }
   class _RegisterPageState extends State<RegisterPage>{
-         
+ 
 @override
-
 Widget build(BuildContext context) {
- return Scaffold(
-  backgroundColor: Colors.black,
-   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  return Scaffold(
+    backgroundColor: Color(0xFF221112),
+    body: Align(
+      alignment: Alignment.topCenter,
+      child: Padding(
+        padding: const EdgeInsets.only(top: 90),
+        child: Text(
+          'Register Now',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontFamily: 'Be Vietnam Pro',
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    ),
+  );
 }
   }
-
