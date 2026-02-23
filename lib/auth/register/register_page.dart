4b205db050e691cf3fef65073c9adc7e9e3b1904
tubaunlu <tuba.unlu.011@gmail.com>
2026-02-23 16:21:14 +0300

    update
    commit

--- lib/auth/register/register_page.dart
+++ lib/auth/register/register_page.dart
@@ -8,18 +8,19 @@ class RegisterPage extends StatefulWidget {
     State<RegisterPage> createState() => _RegisterPageState();  
   }
   class _RegisterPageState extends State<RegisterPage>{
- 
+
 @override
 Widget build(BuildContext context) {
   return Scaffold(
     backgroundColor: Color(0xFF221112),
-    body: Align(
-      alignment: Alignment.topCenter,
-      child: Padding(
-        padding: const EdgeInsets.only(top: 90),
-        child: Text(
-          'Register Now',
-          textAlign: TextAlign.center,
+    body: Padding(
+        padding: const EdgeInsets.only(top: 70),
+        child: Column(
+           mainAxisAlignment: MainAxisAlignment.start,
+           crossAxisAlignment: CrossAxisAlignment.center,
+         children: [
+          Text(
+          'Sign up',
           style: TextStyle(
             fontFamily: 'Be Vietnam Pro',
             fontSize: 28,
@@ -27,8 +28,110 @@ Widget build(BuildContext context) {
             color: Colors.white,
           ),
         ),
+        SizedBox(height: 20),
+        
+      Padding(
+        padding: EdgeInsets.symmetric(vertical: 20.0,horizontal: 20.0),
+      child: TextField(
+      decoration: InputDecoration(
+      labelText: 'Username',
+      labelStyle: TextStyle(color:Color(0xFFC89295)),
+      filled: true,
+      fillColor: Color(0xFF472426),
+      border: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(12),
+      borderSide: BorderSide.none,
+      ),
+      contentPadding: EdgeInsets.all(18),
+      ),
+      style: TextStyle(color: Colors.white),
+    
+      ) 
+      ),
+    
+      Padding(padding: 
+      EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+      child:TextField(
+        decoration: InputDecoration(
+      labelText: 'Email address',
+      labelStyle: TextStyle(color: Color(0xFFC89295)),
+      filled: true,
+      fillColor: Color(0xFF472426),
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(12),
+        borderSide: BorderSide.none,   
+      ),
+      contentPadding: EdgeInsets.all(18),
+        ),
+      style: TextStyle(color: Colors.white),
+      )
+      ),
+    
+     Padding(padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+    child: TextField(
+      decoration: InputDecoration(
+        labelText: 'Password',
+        labelStyle: TextStyle(color: Color(0xFFC89295)),
+        filled: true,
+        fillColor: Color(0xFF472426),
+      border: OutlineInputBorder(
+     borderRadius: BorderRadius.circular(12),
+     borderSide: BorderSide.none,
+      ),
+      contentPadding: EdgeInsets.all(18),
+      ),
+     style: TextStyle(color: Colors.white),
       ),
     ),
+ 
+    SizedBox(height: 20),
+    ElevatedButton(
+     onPressed: () {},  
+     style: ElevatedButton.styleFrom( 
+     backgroundColor: Colors.red, 
+    padding: EdgeInsets.symmetric(vertical:20, horizontal: 140),
+   shape: RoundedRectangleBorder(
+   borderRadius: BorderRadius.circular(30),
+  )
+ ),
+      child: const Text(
+    'Sign up',
+     style: TextStyle(
+     fontWeight: FontWeight.bold,
+     fontSize: 18,
+     color: Colors.white,
+ ),
+),
+ ),  
+    SizedBox(height: 20),
+    Text("Already have an account?",
+    style: TextStyle(
+    color: Color(0xFFC89295),
+    fontSize: 15,
+ ),
+ ),
+
+  SizedBox(height: 20),
+   ElevatedButton(
+  onPressed:() {},  
+  style: ElevatedButton.styleFrom(
+  backgroundColor: Color(0xFF472426),
+  padding: EdgeInsets.symmetric(vertical: 20, horizontal: 140),
+  shape: RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(30),
+  )
+  ),
+  child: const Text(
+ 'Log in',
+ style: TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 18,
+  color: Colors.white,
+ ),
+  ),
+  )  
+      ]
+      )     
+     ),
   );
-}
-  }
+}}
